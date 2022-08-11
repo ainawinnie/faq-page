2ae8accbe7cad5964f0da1b0da8251841a3003ea
Aina <aina.winnie@gmail.com>
2022-08-10 22:18:42 -0300

    Inclui exibição da resposta selecionada

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@ const questions = document.querySelectorAll('.question')
 const questionTab = document.querySelectorAll('.question-group')
 const answers = document.querySelectorAll('.answer')
 
-console.log(questionTab[1].classList)
-
-questionTab.forEach((question)=>{
-    question.addEventListener('click',showAnswer)
+questionTab.forEach((question) => {
+    question.addEventListener('click', () => {
+        questionTab.forEach((item) => {
+            if (item !== question) {
+                const answer = item.querySelectorAll('.answer')
+                answer[0].style.display = 'none'
+                console.log('aqui')
+            } else {
+                const answer = item.querySelectorAll('.answer')
+                answer[0].style.display = 'flex'
+                console.log(answer[0])
+            }
+        })
+    })
 })
-
-function showAnswer(e){
-
-    console.log(e.classLists)
-    
-}
