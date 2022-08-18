07334ea9d188bd41b9b2f8bc156b8e3e9a811fdc
Aina <aina.winnie@gmail.com>
2022-08-18 20:31:39 -0300

    Ajusta versão mobile

--- js/index.js
+++ js/index.js
@@ -6,12 +6,15 @@ questionTab.forEach((question) => {
     question.addEventListener('click', () => {
         questionTab.forEach((item) => {
             if (item !== question) {
-                const answer = item.querySelectorAll('.answer')
-                answer[0].style.display = 'none'
-                console.log('aqui')
+                const answer = item.querySelectorAll('.answer');
+                const arrow = item.querySelectorAll('.open-arrow');
+                answer[0].style.display = 'none';
+                arrow[0].style.rotate = '0deg';
             } else {
-                const answer = item.querySelectorAll('.answer')
-                answer[0].style.display = 'flex'
+                const answer = item.querySelectorAll('.answer');
+                const arrow = item.querySelectorAll('.open-arrow');
+                answer[0].style.display = 'flex';
+                arrow[0].style.rotate = '180deg';
                 console.log(answer[0])
             }
         })
